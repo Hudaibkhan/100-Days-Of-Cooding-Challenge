93a6b5b0c74f4c0f5bc11f4209ab1d1682030f3e
Hudaibkhan <161836104+Hudaibkhan@users.noreply.github.com>
2024-03-05 21:51:45 +0500

    100-Days-Cooding-Challenge : Day 1

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+// Question :1
+// Installing comlete
+// Question:2
+// let personName:string ="Hudaib";
+// console.log(`Hello ${personName}, would you like to learn some Typescript Today?`);
+// Question: 3
+// lowercase
+// let personName:string= 'Hudaib';
+// console.log("lowercase:", personName.toLowerCase());
+// uppercase
+// console.log("uppercase:", personName.toLocaleUpperCase());
+// titlecase
+// console.log("titilecase:", personName.replace(/\bw,/g,c=> c.toLocaleUpperCase()));
